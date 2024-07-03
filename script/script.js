a9ced2cdad2fd9c7bf474e91fa0f812c6c7ea56c
Ilyina <Weightsword@outlook.com>
2024-07-03 20:19:57 +0800

    run git log

--- script/script.js
+++ script/script.js
@@ -1,1 +1,19 @@
+const { exec } = require('child_process');
+
+// 定义一个函数来执行 Git 命令
+function runGitCommand(command) {
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`执行命令时发生错误: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+}
+
+runGitCommand('git log');
 console.log('Hello world!');
